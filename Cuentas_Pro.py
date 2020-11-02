55d8eafdf5c63c247396f701f8679934cf22613e
AndresRubio45 <69913108+AndresRubio45@users.noreply.github.com>
2020-11-01 23:45:10 -0500

    Update Cuentas_Pro.py

--- Cuentas_Pro.py
+++ Cuentas_Pro.py
@@ -5,6 +5,8 @@ Created on Sun Nov  1 05:13:38 2020
 @author: Carlos Rubio
 """
 
+#Definición de la clase Cuenta con sus atributos
+
 class Cuenta:
     
     def __init__(self):
@@ -15,7 +17,9 @@ class Cuenta:
         self.A_pagar = 0
         self.Efectivo = 0
         self.Por_Pagar = 0
-        
+ 
+ #Para cuando se adicione una transacción a la cuenta       
+ 
     def __add__(self,monto,contado):
         self.Subtotal = self.Subtotal + monto
         self.Iva = self.Subtotal * 0.19
@@ -25,7 +29,7 @@ class Cuenta:
         self.Efectivo = self.A_pagar * (contado/100)
         self.Por_Pagar = self.A_pagar * ((100-contado)/100)
             
-##Promedio ponderado
+#Creación de las cuentas más importantes
 
 Compras_Administrativas = Cuenta()
 Compras_Comercialización = Cuenta()
@@ -33,24 +37,30 @@ Compras_Producción = Cuenta()
 Inventario_Materias_Primas = Cuenta()
 Inventario_Productos_Terminados = Cuenta()
 
+#Creación de la clase Saldo para cuando se realice una contabilidad más detallada
+#de los inventarios
+
 class Saldo:
     
     def __init__(self):
         self.costo = 0
         self.cantidad = 0
         self.valor = 0
-        
+
+#Para cuando se adicione una transacción a la cuenta 
+
     def sumar(self,cantidad,precio):
         self.cantidad = self.cantidad + cantidad
         self.valor = self.valor + (precio * cantidad)
         self.costo = self.valor / self.cantidad
-    
-
-        
-#Propiedades, planta y equipo
+          
+#Diccionario de la vida útil de las propiedades
 
 vida_util = {"Vehiculos":5,"Edificios":20,"Muebles y enseres":10,"Equipo de cómputo":3,
              "Maquinaria y equipo":10}
+
+#Creación de la clase Propiedad junto con el método para calcular su
+#respectiva depreciación.
 
 class Propiedad:
     
